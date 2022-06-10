96d0b6ebaf814f00b867f89277f9894010bc0dc2
josuerojasrojas <josuerojas.rojas@gmail.com>
2022-06-10 16:14:03 -0400

    [FEAT] Update kit headings

--- src/constants/kits.ts
+++ src/constants/kits.ts
@@ -5,45 +5,35 @@ export const foodKit: CarouselProps = {
   title: 'How to make a Food Kit',
   cards: [
     {
-      heading: 'Get Wet',
+      heading: 'Peanut Butter',
       imageSrc: logo,
       text: 'Fill a supernatural bottle with filtered water until it reaches the white line.'
     },
     {
-      heading: 'Join Forces',
+      heading: 'Snack Puddings',
       imageSrc: logo,
       text: 'Pour the entire vial of concetrate into its conesting supernatural bottle.'
     },
     {
-      heading: 'Embrace Nature',
+      heading: 'Ramen Noodles',
       imageSrc: logo,
       text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
     },
     {
-      heading: '2 Snack Puddings',
+      heading: 'Bottle of Gatorade',
       imageSrc: logo,
       text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
     },
     {
-      heading: '2 Ramen Noodle Packages',
+      heading: 'Spork',
       imageSrc: logo,
       text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
     },
     {
-      heading: '1 Plastic Gatorade',
+      heading: 'Ziploc Storage Bafs',
       imageSrc: logo,
       text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
     },
-    {
-      heading: '1 Spork',
-      imageSrc: logo,
-      text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
-    },
-    {
-      heading: '1 gallon zip-loc storage bag',
-      imageSrc: logo,
-      text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
-    }
   ]
 };
 
@@ -51,18 +41,34 @@ export const shelterKit: CarouselProps = {
   title: 'How to make a Shelter Kit',
   cards: [
     {
-      heading: '1 gallon zip-loc storage bag',
+      heading: 'Mylar Emergency Blanket',
       imageSrc: logo,
       text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
     },    {
-      heading: '1 gallon zip-loc storage bag',
+      heading: 'Pair of Socks',
       imageSrc: logo,
       text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
-    },    {
-      heading: '1 gallon zip-loc storage bag',
+    },
+    {
+      heading: 'Watch Cap',
       imageSrc: logo,
       text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
-    }
+    },
+    {
+      heading: 'Pair of Gloves',
+      imageSrc: logo,
+      text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
+    },
+    {
+      heading: 'Waterproof Tarp',
+      imageSrc: logo,
+      text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
+    },
+    {
+      heading: 'Ziplock Storage Bags',
+      imageSrc: logo,
+      text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
+    },
   ]
 };
 
@@ -70,15 +76,31 @@ export const firstAidKit: CarouselProps = {
   title: 'How to make a First Aid Kit',
   cards: [
     {
-      heading: '1 gallon zip-loc storage bag',
+      heading: 'Regular Band Aids',
       imageSrc: logo,
       text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
     },    {
-      heading: '1 gallon zip-loc storage bag',
+      heading: '2x2 Gauze Bandages',
       imageSrc: logo,
       text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
-    },    {
-      heading: '1 gallon zip-loc storage bag',
+    },
+    {
+      heading: 'Antibacterial Wipes',
+      imageSrc: logo,
+      text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
+    },
+    {
+      heading: 'Lip Balm',
+      imageSrc: logo,
+      text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
+    },
+    {
+      heading: 'Moleskin Sheet',
+      imageSrc: logo,
+      text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
+    },
+    {
+      heading: 'Ziplock Storage Bags',
       imageSrc: logo,
       text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
     }
@@ -89,15 +111,47 @@ export const hygieneKit: CarouselProps = {
   title: 'How to make a Hygiene Kit',
   cards: [
     {
-      heading: '1 gallon zip-loc storage bag',
+      heading: 'Soap',
       imageSrc: logo,
       text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
-    },    {
-      heading: '1 gallon zip-loc storage bag',
+    },
+    {
+      heading: 'Shampoo',
       imageSrc: logo,
       text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
-    },    {
-      heading: '1 gallon zip-loc storage bag',
+    },
+    {
+      heading: 'Tooth Brush',
+      imageSrc: logo,
+      text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
+    },
+    {
+      heading: 'Toothpaste',
+      imageSrc: logo,
+      text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
+    },
+    {
+      heading: 'Toilet Paper Pack',
+      imageSrc: logo,
+      text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
+    },
+    {
+      heading: 'Stick Deodorant',
+      imageSrc: logo,
+      text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
+    },
+    {
+      heading: 'Wash Cloth',
+      imageSrc: logo,
+      text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
+    },
+    {
+      heading: 'Disposable Razor',
+      imageSrc: logo,
+      text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
+    },
+    {
+      heading: 'Ziplock Storage Bag',
       imageSrc: logo,
       text: 'Rotate the nozzle, spritz, take a deep breath and begin your cleaning ritual.'
     }
